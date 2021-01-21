a9928cfbdf143e36426fcb0cf1499bc395314359
sudodou <wsdosh@gmail.com>
2021-01-21 15:19:12 +0800

    Update build.sh

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -5,7 +5,7 @@ sed -i 's/git\.openwrt\.org\/project\/luci/github\.com\/openwrt\/luci/g' ./feeds
 ./scripts/feeds install luci
 mv ./bin/luci-app-shortcutmenu ./package/
 make defconfig
-make package/luci-app-wrtbwmon/compile V=s -j$(nproc) BUILD_LOG=1
+make package/luci-app-shortcutmenu/compile V=s -j$(nproc) BUILD_LOG=1
 
 tar -cJf logs.tar.xz logs
 mv logs.tar.xz bin
